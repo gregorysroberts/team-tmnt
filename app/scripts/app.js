409bf85379fdc711ff38949efc2d44d4440920d5
Greg Roberts <gregorysroberts@gmail.com>
2014-02-05 19:37:58 -0700

    sets up ui.router

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('teamTmntApp', [
+	'ui.router'
+])
+	.config(function ($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/state1");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('main', {
+      url: "/",
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .state('state1.list', {
+      url: "/list",
+      templateUrl: "partials/state1.list.html",
+      controller: function($scope) {
+        $scope.items = ["A", "List", "Of", "Items"];
+      }
+    })
+    .state('state2', {
+      url: "/state2",
+      templateUrl: "partials/state2.html"
+    })
+    .state('state2.list', {
+      url: "/list",
+        templateUrl: "partials/state2.list.html",
+        controller: function($scope) {
+          $scope.things = ["A", "Set", "Of", "Things"];
+        }
+      })
+    });
